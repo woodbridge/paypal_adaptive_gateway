ae0affe00794d0157fc398a187e84aef2bb1d2d2
MattG <matt@madebylamp.com>
2010-05-06 18:01:28 +0100

    updates

--- lib/active_merchant/billing/gateways/paypal_adaptive_payments/version.rb
+++ lib/active_merchant/billing/gateways/paypal_adaptive_payments/version.rb
@@ -2,7 +2,7 @@ module ActiveMerchant
   module Billing
     class PaypalAdaptivePaymentGateway < Gateway # :nodoc
       def version
-        {:major => 0, :minor => 1, :patchlevel => 3}
+        {:major => 0, :minor => 1, :patchlevel => 4}
       end
     end
   end
