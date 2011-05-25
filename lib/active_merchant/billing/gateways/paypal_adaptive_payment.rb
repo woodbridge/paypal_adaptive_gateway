e99999d544482b5773c129d32318078d747f0a20
Brian McKinney <brian@realitycontainment.com>
2011-05-25 16:46:31 -0600

    Change RAILS_ROOT to Rails.root. Make whitespac consistent and put parens around methods definitions

--- lib/active_merchant/billing/gateways/paypal_adaptive_payment.rb
+++ lib/active_merchant/billing/gateways/paypal_adaptive_payment.rb
@@ -9,25 +9,25 @@ require 'json/add/rails'
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class PaypalAdaptivePaymentGateway < Gateway # :nodoc:
-      
+
       include AdaptivePaymentResponses
       include AdaptiveUtils
-      
+
       TEST_URL = 'https://svcs.sandbox.paypal.com/AdaptivePayments/'
       LIVE_URL = 'https://svcs.paypal.com/AdaptivePayments/'
-      
+
       # The countries the gateway supports merchants from as 2 digit ISO country codes
       self.supported_countries = ['US']
-      
+
       # The homepage URL of the gateway
       self.homepage_url = 'http://x.com/'
-      
+
       # The name of the gateway
       self.display_name = 'Paypal Adaptive Payments'
-      
+
       attr_accessor :config_path
-      @config_path = "#{RAILS_ROOT}/config/paypal.yml"
-      
+      @config_path = "#{Rails.root}/config/paypal.yml"
+
       def initialize(options = {})
         @config = {}
         if options.empty?
@@ -35,17 +35,17 @@ module ActiveMerchant #:nodoc:
         else
           @config.merge! options
         end
-      end 
-      
+      end
+
       def pay(options)
         commit 'Pay', build_adaptive_payment_pay_request(options)
-      end                       
-    
-      def details_for_payment options
+      end
+
+      def details_for_payment(options)
         commit 'PaymentDetails', build_adaptive_payment_details_request(options)
       end
-      
-      def refund options
+
+      def refund(options)
         commit 'Refund', build_adaptive_refund_details(options)
       end
 
@@ -62,36 +62,36 @@ module ActiveMerchant #:nodoc:
       # * +:notify_url+ - The URL to which you want all IPN messages for this preapproval to be sent. (Optional)
       #
       # To get more details on fields see +Paypal PreApproval API+ at https://www.x.com/docs/DOC-1419
-      def preapprove_payment options
+      def preapprove_payment(options)
         commit 'Preapproval', build_preapproval_payment(options)
       end
-      
-      def cancel_preapproval options
+
+      def cancel_preapproval(options)
         commit 'CancelPreapproval', build_cancel_preapproval(options)
-      end      
-      
-      def preapproval_details_for options
+      end
+
+      def preapproval_details_for(options)
         commit 'PreapprovalDetails', build_preapproval_details(options)
       end
-      
-      def convert_currency  options
+
+      def convert_currency(options)
         commit 'ConvertCurrency', build_currency_conversion(options)
       end
-      
+
       #debug method, provides an easy to use debug method for the class
       def debug
         "Url: #{@url}\n\n JSON: #{@xml} \n\n Raw: #{@raw}"
       end
-      
-      private                       
-      
+
+      private
+
       #loads config from default file if it is not provided to the constructor
       def load_config
         raise ConfigDoesNotExist if !File.exists?(@config_path);
         @config.merge! Yaml.load_file(@config_path)[RAILS_ENV].symbolize_keys!
       end
-      
-      def build_adaptive_payment_pay_request opts
+
+      def build_adaptive_payment_pay_request(opts)
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
@@ -101,14 +101,14 @@ module ActiveMerchant #:nodoc:
             x.errorLanguage opts[:error_language] ||= 'en_US'
           end
           x.actionType 'PAY'
-	  x.senderEmail opts[:senderEmail]
+          x.senderEmail opts[:senderEmail]
           x.cancelUrl opts[:cancel_url]
           x.returnUrl opts[:return_url]
           if opts[:notify_url]
             x.ipnNotificationUrl opts[:notify_url]
           end
           x.memo opts[:memo] if opts[:memo]
-	  x.feesPayer opts[:feesPayer] if opts[:feesPayer]
+          x.feesPayer opts[:feesPayer] if opts[:feesPayer]
           x.pin opts[:pin] if opts[:pin]
           x.currencyCode opts[:currency_code] ||= 'USD'
           x.receiverList do |x|
@@ -125,21 +125,21 @@ module ActiveMerchant #:nodoc:
           x.reverseAllParallelPaymentsOnError opts[:reverse_all_parallel_payments_on_error] || 'false'
          end
       end
-      
-      def build_adaptive_payment_details_request opts
+
+      def build_adaptive_payment_details_request(opts)
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
-        xml.PayRequest do |x|          
+        xml.PayRequest do |x|
           x.requestEnvelope do |x|
             x.detailLevel 'ReturnAll'
             x.errorLanguage opts[:error_language] ||= 'en_US'
           end
           x.payKey opts[:paykey]
-          end
+        end
       end
-      
-      def build_adaptive_refund_details options
+
+      def build_adaptive_refund_details(options)
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
@@ -172,15 +172,15 @@ module ActiveMerchant #:nodoc:
             end
           end
           x.feesPayer options[:fees_payer] ||= 'EACHRECEIVER'
-	end
+        end
       end
-      
-      def build_preapproval_payment options
+
+      def build_preapproval_payment(options)
         opts = {
           :currency_code => "USD",
           :start_date => DateTime.current
         }.update(options)
-        
+
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
@@ -189,26 +189,26 @@ module ActiveMerchant #:nodoc:
           x.requestEnvelope do |x|
             x.detailLevel 'ReturnAll'
             x.errorLanguage opts[:error_language] ||= 'en_US'
-	    x.senderEmail opts[:senderEmail]
+            x.senderEmail opts[:senderEmail]
           end
-          
+
           # required preapproval fields
           x.endingDate opts[:end_date].strftime("%Y-%m-%dT%H%%3a%M%%3a%S")
           x.startingDate opts[:start_date].strftime("%Y-%m-%dT%H%%3a%M%%3a%S")
           x.maxTotalAmountOfAllPayments opts[:max_amount]
-	  x.maxNumberOfPayments opts[:maxNumberOfPayments]
+          x.maxNumberOfPayments opts[:maxNumberOfPayments]
           x.currencyCode options[:currency_code]
           x.cancelUrl opts[:cancel_url]
           x.returnUrl opts[:return_url]
-          
+
           # notify url
           if opts[:notify_url]
             x.ipnNotificationUrl opts[:notify_url]
           end
-	end
+        end
       end
-      
-      def build_preapproval_details options
+
+      def build_preapproval_details(options)
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
@@ -221,8 +221,8 @@ module ActiveMerchant #:nodoc:
           x.getBillingAddress options[:get_billing_address] if options[:get_billing_address]
         end
       end
-      
-      def build_cancel_preapproval options
+
+      def build_cancel_preapproval(options)
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
@@ -233,9 +233,9 @@ module ActiveMerchant #:nodoc:
           end
           x.preapprovalKey options[:preapproval_key]
         end
-      end      
-      
-      def build_currency_conversion options
+      end
+
+      def build_currency_conversion(options)
         @xml = ''
         xml = Builder::XmlMarkup.new :target => @xml, :indent => 2
         xml.instruct!
@@ -245,35 +245,35 @@ module ActiveMerchant #:nodoc:
             x.errorLanguage options[:error_language] ||= 'en_US'
           end
           x.baseAmountList do |x|
-	  options[:currency_list].each do |currency|
-            x.currency do |x|
-              x.amount currency[:amount]
-              x.code currency[:code ]
+            options[:currency_list].each do |currency|
+              x.currency do |x|
+                x.amount currency[:amount]
+                x.code currency[:code ]
+              end
             end
-	   end
-	  end
+          end
           x.convertToCurrencyList do |x|
-	      options[:to_currencies].each do |k,v| 
+            options[:to_currencies].each do |k,v| 
               x.currencyCode "#{v}"
             end
           end
-  end
+        end
       end
-      
-      def parse json
+
+      def parse(json)
         @raw = json
-	  resp = JSON.parse json
+        resp = JSON.parse(json)
         if resp['responseEnvelope']['ack'] == 'Failure'
           error = AdaptivePaypalErrorResponse.new(json)
         else
           AdaptivePaypalSuccessResponse.new(resp)
         end
-      end     
-      
+      end
+
       def commit(action, data)
         @response = parse(post_through_ssl(action, data))
       end
-      
+
       def post_through_ssl(action, parameters = {})
         headers = {
           "X-PAYPAL-REQUEST-DATA-FORMAT" => "XML",
@@ -292,19 +292,19 @@ module ActiveMerchant #:nodoc:
         server.use_ssl = true
         server.start { |http| http.request(request) }.body
       end
-      
+
       def endpoint_url
         test? ? TEST_URL : LIVE_URL
       end
-      
+
       def test?
         Base.gateway_mode == :test
       end
-      
+
       def url action
         @url = URI.parse(endpoint_url + action)
       end
-      
+
     end
   end
 end
